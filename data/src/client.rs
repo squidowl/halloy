0bf2929bde555851769f05a0cd5caf17d99e1fff
Cory Forsstrom <cforsstrom18@gmail.com>
2022-04-24 10:19:17 -0700

    Add message conversion and map

--- data/src/client.rs
+++ data/src/client.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::message::Message;
 
 #[derive(Debug, Clone)]
@@ -20,3 +22,28 @@ pub struct Ready {
 
 #[derive(Debug, Clone)]
 pub struct Sender(irc::client::Sender);
+
+#[derive(Debug, Clone, Default)]
+pub struct Map(HashMap<String, State>);
+
+impl Map {
+    pub fn disconnected(&mut self, server: String) {
+        self.0.insert(server, State::Disconnected);
+    }
+
+    pub fn ready(&mut self, server: String, sender: Sender) {
+        self.0.insert(
+            server,
+            State::Ready(Ready {
+                sender,
+                messages: vec![],
+            }),
+        );
+    }
+
+    pub fn add_message(&mut self, server: &str, message: Message) {
+        if let Some(State::Ready(ready)) = self.0.get_mut(server) {
+            ready.messages.push(message);
+        }
+    }
+}
