7946a6a042143da88023f0a264ebc94aadf0a98b
Cory Forsstrom <cforsstrom18@gmail.com>
2023-07-28 09:54:15 -0700

    move files under linux assets folder

--- scripts/generate-icons.sh
+++ scripts/generate-icons.sh
@@ -7,4 +7,4 @@ src=logo.png
 conv_opts="-colors 256 -background none -density 300"
 
 # the linux icon
-convert $conv_opts -resize "!128x128" "$src" "logo_128px.png"
+convert $conv_opts -resize "!128x128" "$src" "linux/org.squidowl.halloy.png"
