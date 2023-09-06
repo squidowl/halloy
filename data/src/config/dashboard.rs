e322338ea7c737a95b1decd582f80a0d36a60b36
Casper Rogild Storm <casper+github@rogildstorm.com>
2023-09-06 08:28:32 +0200

    custom width sidebar

--- data/src/config/dashboard.rs
+++ data/src/config/dashboard.rs
@@ -5,5 +5,26 @@ use crate::dashboard::DefaultAction;
 #[derive(Debug, Copy, Default, Clone, Deserialize)]
 pub struct Dashboard {
     #[serde(default)]
-    pub sidebar_default_action: DefaultAction,
+    pub sidebar: Sidebar,
+}
+
+#[derive(Debug, Copy, Clone, Deserialize)]
+pub struct Sidebar {
+    #[serde(default)]
+    pub default_action: DefaultAction,
+    #[serde(default = "default_sidebar_width")]
+    pub width: u16,
+}
+
+impl Default for Sidebar {
+    fn default() -> Self {
+        Sidebar {
+            default_action: Default::default(),
+            width: default_sidebar_width(),
+        }
+    }
+}
+
+fn default_sidebar_width() -> u16 {
+    120
 }
