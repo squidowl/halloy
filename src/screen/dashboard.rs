e3a822647a2a0ef0d5bf9d50dfa9e7f2302f35ca
Andrew Baldwin <andrew@boldfriend.org>
2023-12-10 22:20:30 -0800

    Ensure a pane is focused on launch, if possible.

--- src/screen/dashboard.rs
+++ src/screen/dashboard.rs
@@ -64,7 +64,12 @@ impl Dashboard {
     pub fn restore(dashboard: data::Dashboard) -> (Self, Command<Message>) {
         let mut dashboard = Dashboard::from(dashboard);
 
-        let command = dashboard.track();
+        let command;
+        if let Some((pane, _)) = dashboard.panes.panes.iter().next() {
+            command = Command::batch(vec![dashboard.focus_pane(*pane),dashboard.track()]);
+        } else {
+            command = dashboard.track();
+        }
 
         (dashboard, command)
     }
