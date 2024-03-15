78310d7c4703167de7150de00ef6169a43dfcc9e
Cory Forsstrom <cforsstrom18@gmail.com>
2024-03-15 12:50:48 -0700

    Even more icon sizes

--- scripts/generate-icons.sh
+++ scripts/generate-icons.sh
@@ -7,7 +7,7 @@ src=logo.png
 conv_opts="-colors 256 -background none -density 300"
 
 # the linux icon
-for size in "32" "64" "128" "256" "512"; do
+for size in "16" "24" "32" "48" "64" "96" "128" "256" "512"; do
   target="linux/icons/hicolor/${size}x${size}/apps"
   mkdir -p "$target"
   convert $conv_opts -resize "!${size}x${size}" "$src" "$target/org.squidowl.halloy.png"
