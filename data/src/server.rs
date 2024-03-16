ca19b062e2dee7551762003cced14d6f007d5b8e
Funkeleinhorn <git@funkeleinhorn.com>
2024-03-16 04:43:29 +0100

    Make passwords readable from file
    It would be nice to be able to put passwords in extra files with
    secure permissions instead of including them in plaintext in the config
    as some users might share the config via version control etc. and this
    would leak passwords.

--- data/src/server.rs
+++ data/src/server.rs
@@ -1,9 +1,12 @@
 use std::collections::BTreeMap;
 use std::fmt;
+use std::fs;
 
 use serde::{Deserialize, Serialize};
 
 use crate::config;
+use crate::config::Error;
+use crate::config::server::Sasl;
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct Server(String);
@@ -45,5 +48,37 @@ impl Map {
 
     pub fn entries(&self) -> impl Iterator<Item = Entry> + '_ {
         self.0.iter().map(Entry::from)
+    }
+
+    pub fn read_password_files(&mut self) -> Result<(), Error> {
+        for (_, config) in self.0.iter_mut() {
+            if let Some(pass_file) = &config.password_file {
+                if config.password.is_some() {
+                    return Err(Error::Parse("Only one of password and password_file can be set.".to_string()));
+                }
+                let pass = fs::read_to_string(pass_file)?;
+                config.password = Some(pass);
+            }
+            if let Some(nick_pass_file) = &config.nick_password_file {
+                if config.nick_password.is_some() {
+                    return Err(Error::Parse("Only one of nick_password and nick_password_file can be set.".to_string()));
+                }
+                let nick_pass = fs::read_to_string(nick_pass_file)?;
+                config.nick_password = Some(nick_pass);
+            }
+            if let Some(sasl) = &mut config.sasl {
+                match sasl {
+                    Sasl::Plain { password: password @ None, password_file: Some(pass_file), .. } => {
+                        let pass = fs::read_to_string(pass_file)?;
+                        *password = Some(pass);
+                    },
+                    Sasl::Plain { password: Some(_), password_file: None, .. } => {},
+                    _ => {
+                        return Err(Error::Parse("Exactly one of sasl.plain.password or sasl.plain.password_file must be set.".to_string()));
+                    }
+                }
+            }
+        }
+        Ok(())
     }
 }
