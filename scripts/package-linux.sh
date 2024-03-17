4a6e3ba6919eb30b5b37e5578e345bedbbdc1512
Andrew Baldwin <andrew@boldfriend.org>
2024-03-16 17:31:04 -0700

    Simple fix for #279, agnostic to which icon sizes are included in the future.

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -27,6 +27,10 @@ package() {
   build
 
   install -D $ASSETS_DIR/* -t $ARCHIVE_DIR
+  for ASSET_PATH in $ASSETS_DIR/icons/hicolor/*/apps/org.squidowl.halloy.png; do
+    ASSET_ARCHIVE_PATH="$ARCHIVE_DIR/${ASSET_PATH#"$ASSETS_DIR"}"
+    install -Dm644 $ASSET_PATH $ASSET_ARCHIVE_PATH
+  done
   install -Dm755 $BINARY $ARCHIVE_DIR
   tar czvf $ARCHIVE_PATH -C $ARCHIVE_DIR .
 
