4f5349fd07a49d4682c99b4594ad41a229961b9a
Peter Gammelgaard Poulsen <peter.gammelgaard@gmail.com>
2024-03-28 00:58:27 +0100

    Added script to package app

---
+++ scripts/package-macos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RELEASE_DIR="target/release"
+APP_DIR="$RELEASE_DIR/macos"
+APP_NAME="Halloy.app"
+DMG_NAME="halloy.dmg"
+DMG_DIR="$RELEASE_DIR/macos"
+
+# package dmg
+echo "Packing disk image..."
+ln -sf /Applications "$DMG_DIR/Applications"
+hdiutil create "$DMG_DIR/$DMG_NAME" -volname "Halloy" -fs HFS+ -srcfolder "$APP_DIR" -ov -format UDZO
+echo "Packed '$APP_NAME' in '$APP_DIR'"
