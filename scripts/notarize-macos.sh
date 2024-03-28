5a6c51567d9060977e2384b0f5f06d769c9380f9
Peter Gammelgaard Poulsen <peter.gammelgaard@gmail.com>
2024-03-28 01:11:12 +0100

    Fixed notarize script environment check

--- scripts/notarize-macos.sh
+++ scripts/notarize-macos.sh
@@ -5,7 +5,7 @@ APP_DIR="$RELEASE_DIR/macos"
 APP_NAME="Halloy.app"
 APP_PATH=$APP_DIR/$APP_NAME
 
-environment=("MACOS_NOTARIZATION_APPLE_ID MACOS_NOTARIZATION_TEAM_ID MACOS_NOTARIZATION_PWD")
+environment=("MACOS_NOTARIZATION_APPLE_ID" "MACOS_NOTARIZATION_TEAM_ID" "MACOS_NOTARIZATION_PWD")
 for var in "${environment[@]}"; do
     if [[ -z "${!var}" ]]; then
         echo "Error: $var is not set"
