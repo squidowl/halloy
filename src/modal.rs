d0368ae709c26c6f464ec999a130aa9e765b013b
Cory Forsstrom <cforsstrom18@gmail.com>
2024-04-19 12:45:40 -0700

    Move Modal to it's own module & prevent diff

--- src/modal.rs
+++ src/modal.rs
@@ -1,12 +1,25 @@
+use data::config;
+
+use crate::widget::Element;
+
 pub mod reload_configuration_error;
 
-#[derive(Clone, Debug)]
-pub enum Message {
-    ReloadConfigurationError(reload_configuration_error::Message)
+#[derive(Debug)]
+pub enum Modal {
+    ReloadConfigurationError(config::Error),
 }
 
-impl From<reload_configuration_error::Message> for Message {
-    fn from(message: reload_configuration_error::Message) -> Self {
-        Self::ReloadConfigurationError(message)
+impl Modal {
+    pub fn view(&self) -> Element<Message> {
+        match self {
+            Modal::ReloadConfigurationError(error) => {
+                reload_configuration_error::view(error).map(Message::ReloadConfigurationError)
+            }
+        }
     }
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    ReloadConfigurationError(reload_configuration_error::Message),
 }
