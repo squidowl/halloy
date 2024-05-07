b947d3b92280b8e89e4786aa68dbf81114656bbe
Kai Korla <kai@korla.cloud>
2024-05-07 11:53:08 +0200

    Add support for Windows binary meta information and fix WiX installer version

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -4,8 +4,10 @@
 #!/bin/bash
 EXE_NAME="halloy.exe"
 TARGET="x86_64-pc-windows-msvc"
+VERSION=$(cat VERSION).0
 
 # build binary
 rustup target add $TARGET
+cargo-set-version set-version $VERSION
 cargo build --release --target=$TARGET
 cp -fp target/$TARGET/release/$EXE_NAME target/release
