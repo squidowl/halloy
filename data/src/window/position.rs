f08b8a7a84295cebfc79e409154059c9d835e0a0
Casper Rogild Storm <casper+github@rogildstorm.com>
2024-06-06 21:10:44 +0200

    feat: save window pos and size

---
+++ data/src/window/position.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Copy, Clone, Default, Deserialize, Serialize)]
+pub struct Position {
+    x: f32,
+    y: f32
+}
+
+impl Position {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x,  y }
+    }
+}
+
+impl From<Position> for iced_core::Point {
+    fn from(position: Position) -> Self {
+        Self {
+            x: position.x,
+            y: position.y,
+        }
+    }
+}
+
+impl From<Position> for iced_core::window::Position {
+    fn from(position: Position) -> Self {
+        Self::Specific(position.into())
+    }
+}
---
+++ data/src/window/size.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Copy, Clone, Deserialize, Serialize)]
+pub struct Size {
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Size {
+    pub const MIN_WIDTH: f32 = 320.0;
+    pub const MIN_HEIGHT: f32 = 200.0;
+
+    pub fn new(width: f32, height: f32) -> Self {
+        Self {
+            width: width.max(Self::MIN_WIDTH),
+            height: height.max(Self::MIN_HEIGHT),
+        }
+    }
+}
+
+impl Default for Size {
+    fn default() -> Self {
+        Self {
+            width: 1024.0,
+            height: 768.0,
+        }
+    }
+}
+
+impl From<Size> for iced_core::Size {
+    fn from(size: Size) -> Self {
+        Self {
+            width: size.width,
+            height: size.height,
+        }
+    }
+}
