a84fc19c07b41fc1c07f78f72d172e73fd8c59d2
Casper Rogild Storm <casper+github@rogildstorm.com>
2024-07-09 22:56:27 +0200

    load sound data in memory

--- data/src/config/notification.rs
+++ data/src/config/notification.rs
@@ -22,3 +22,16 @@ pub struct Notifications {
     #[serde(default)]
     pub file_transfer_request: Notification,
 }
+
+impl Notifications {
+    pub fn load_sound_data(&mut self) {
+        self.connected.sound.as_mut().map(|s| s.load_data());
+        self.disconnected.sound.as_mut().map(|s| s.load_data());
+        self.reconnected.sound.as_mut().map(|s| s.load_data());
+        self.highlight.sound.as_mut().map(|sound| sound.load_data());
+        self.file_transfer_request
+            .sound
+            .as_mut()
+            .map(|s| s.load_data());
+    }
+}
