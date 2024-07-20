df281a71993f6704e1c45f05cfcb193abe40085a
Casper Rogild Storm <casper+github@rogildstorm.com>
2024-07-21 00:00:22 +0200

    Added new halloy icons file

--- src/screen/dashboard/sidebar.rs
+++ src/screen/dashboard/sidebar.rs
@@ -255,11 +255,11 @@ fn buffer_button<'a>(
 
     let row = match &buffer {
         Buffer::Server(server) => row![
-            if connected {
-                icon::globe()
+            icon::connected().style(if connected {
+                theme::text::primary
             } else {
-                icon::wifi_off()
-            },
+                theme::text::error
+            }),
             text(server.to_string())
                 .style(theme::text::primary)
                 .shaping(text::Shaping::Advanced)
