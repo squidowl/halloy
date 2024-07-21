17cfc0f8bdac0074fe50d0366fabd96f56f6de22
Kai Korla <kai@korla.cloud>
2024-07-21 21:28:24 +0200

    - fix build-windows-installer.sh script - fix paths in main.wxs

--- scripts/build-windows-installer.sh
+++ scripts/build-windows-installer.sh
@@ -6,7 +6,7 @@ VERSION=$(cat VERSION)
 dotnet tool install --global wix
 
 # add required wix extension
-wix extension add WixToolset.Ui.wixex
+wix extension add WixToolset.UI.wixext
 
 # build the installer
-wix build -pdbtype none -arch x64 -d PackageVersion=$VERSION main.wxs -o halloy-installer.msi -ext ./.wix/extensions/WixToolset.Ui.wixext/5.0.1/wixext5/WixToolset.UI.wixext.dll
+wix build -pdbtype none -arch x64 -d PackageVersion=$VERSION $WXS_FILE -o halloy-installer.msi -ext WixToolset.UI.wixext
