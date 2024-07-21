d9b882b0a0892d842a1e53bb1e82ec586a8018bf
Kai Korla <kai@korla.cloud>
2024-07-21 21:02:31 +0200

    Fix wix extension path

--- scripts/build-windows-installer.sh
+++ scripts/build-windows-installer.sh
@@ -9,4 +9,4 @@ dotnet tool install --global wix
 wix extension add WixToolset.Ui.wixex
 
 # build the installer
-wix build -pdbtype none -arch x64 -d PackageVersion=$VERSION main.wxs -o halloy-installer.msi -ext .\.wix\extensions\WixToolset.Ui.wixext\5.0.1\wixext5\WixToolset.UI.wixext.dll
+wix build -pdbtype none -arch x64 -d PackageVersion=$VERSION main.wxs -o halloy-installer.msi -ext ./.wix/extensions/WixToolset.Ui.wixext/5.0.1/wixext5/WixToolset.UI.wixext.dll
