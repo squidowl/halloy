d9cae50679d16a2c49f0b8ef214e7184241878fd
Kai Korla <kai@korla.cloud>
2024-07-23 17:33:00 +0200

    Optimize build process for Windows binary and Windows installer

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -4,10 +4,13 @@
 #!/bin/bash
 EXE_NAME="halloy.exe"
 TARGET="x86_64-pc-windows-msvc"
-VERSION=$(cat VERSION).0
+HALLOY_VERSION=$(cat VERSION).0
+
+# update package version on Cargo.toml
+cargo install cargo-edit
+cargo set-version $HALLOY_VERSION
 
 # build binary
 rustup target add $TARGET
-cargo-set-version set-version $VERSION
 cargo build --release --target=$TARGET
 cp -fp target/$TARGET/release/$EXE_NAME target/release
