b98381c519cb1889850f5b9b861e3fff407dda3c
Cory Forsstrom <cforsstrom18@gmail.com>
2024-08-09 13:07:47 -0700

    Convert double click to wrap

--- src/widget/wrap.rs
+++ src/widget/wrap.rs
@@ -1,3 +1,5 @@
+use std::{marker::PhantomData, slice};
+
 use iced::{
     advanced::{self, layout, Widget},
     event, Element,
@@ -9,10 +11,11 @@ pub fn wrap<'a, Message, Theme, Renderer>(
     Wrap::new(element)
 }
 
-pub struct Wrap<'a, Message, Theme, Renderer, OnEvent = (), Layout = ()> {
+pub struct Wrap<'a, Message, Theme, Renderer, OnEvent = (), Layout = (), State = ()> {
     inner: Element<'a, Message, Theme, Renderer>,
     on_event: OnEvent,
     layout: Layout,
+    state: PhantomData<State>,
 }
 
 impl<'a, Message, Theme, Renderer> Wrap<'a, Message, Theme, Renderer> {
@@ -21,38 +24,48 @@ impl<'a, Message, Theme, Renderer> Wrap<'a, Message, Theme, Renderer> {
             inner: inner.into(),
             on_event: (),
             layout: (),
+            state: PhantomData,
         }
     }
 }
 
-impl<'a, Message, Theme, Renderer, OnEvent, Layout>
-    Wrap<'a, Message, Theme, Renderer, OnEvent, Layout>
+impl<'a, Message, Theme, Renderer, OnEvent, Layout, State>
+    Wrap<'a, Message, Theme, Renderer, OnEvent, Layout, State>
 {
-    pub fn on_event<T>(self, on_event: T) -> Wrap<'a, Message, Theme, Renderer, T, Layout>
-    where
-        T: self::OnEvent<'a, Message, Theme, Renderer> + 'a,
-    {
+    pub fn on_event<T>(self, on_event: T) -> Wrap<'a, Message, Theme, Renderer, T, Layout, State> {
         Wrap {
             inner: self.inner,
             layout: self.layout,
+            state: self.state,
             on_event,
         }
     }
 
-    pub fn layout<T>(self, layout: T) -> Wrap<'a, Message, Theme, Renderer, OnEvent, T> {
+    pub fn layout<T>(self, layout: T) -> Wrap<'a, Message, Theme, Renderer, OnEvent, T, State> {
         Wrap {
             inner: self.inner,
             on_event: self.on_event,
+            state: self.state,
             layout,
+        }
+    }
+
+    pub fn state<T>(self) -> Wrap<'a, Message, Theme, Renderer, OnEvent, Layout, T> {
+        Wrap {
+            inner: self.inner,
+            on_event: self.on_event,
+            layout: self.layout,
+            state: PhantomData,
         }
     }
 }
 
-pub trait OnEvent<'a, Message, Theme, Renderer> {
+pub trait OnEvent<'a, Message, Theme, Renderer, State> {
     fn on_event(
         &mut self,
+        state: &mut State,
         inner: &mut Element<'a, Message, Theme, Renderer>,
-        state: &mut advanced::widget::Tree,
+        tree: &mut advanced::widget::Tree,
         event: iced::Event,
         layout: advanced::Layout<'_>,
         cursor: advanced::mouse::Cursor,
@@ -63,14 +76,15 @@ pub trait OnEvent<'a, Message, Theme, Renderer> {
     ) -> event::Status;
 }
 
-impl<'a, Message, Theme, Renderer> OnEvent<'a, Message, Theme, Renderer> for ()
+impl<'a, Message, Theme, Renderer, State> OnEvent<'a, Message, Theme, Renderer, State> for ()
 where
     Renderer: advanced::Renderer + 'a,
 {
     fn on_event(
         &mut self,
+        _state: &mut State,
         inner: &mut Element<'a, Message, Theme, Renderer>,
-        state: &mut advanced::widget::Tree,
+        tree: &mut advanced::widget::Tree,
         event: iced::Event,
         layout: advanced::Layout<'_>,
         cursor: advanced::mouse::Cursor,
@@ -80,14 +94,15 @@ where
         viewport: &iced::Rectangle,
     ) -> event::Status {
         inner.as_widget_mut().on_event(
-            state, event, layout, cursor, renderer, clipboard, shell, viewport,
+            tree, event, layout, cursor, renderer, clipboard, shell, viewport,
         )
     }
 }
 
-impl<'a, T, Message, Theme, Renderer> OnEvent<'a, Message, Theme, Renderer> for T
+impl<'a, T, Message, Theme, Renderer, State> OnEvent<'a, Message, Theme, Renderer, State> for T
 where
     T: Fn(
+            &mut State,
             &mut Element<'a, Message, Theme, Renderer>,
             &mut advanced::widget::Tree,
             iced::Event,
@@ -102,8 +117,9 @@ where
 {
     fn on_event(
         &mut self,
+        state: &mut State,
         inner: &mut Element<'a, Message, Theme, Renderer>,
-        state: &mut advanced::widget::Tree,
+        tree: &mut advanced::widget::Tree,
         event: iced::Event,
         layout: advanced::Layout<'_>,
         cursor: advanced::mouse::Cursor,
@@ -113,14 +129,15 @@ where
         viewport: &iced::Rectangle,
     ) -> event::Status {
         self(
-            inner, state, event, layout, cursor, renderer, clipboard, shell, viewport,
+            state, inner, tree, event, layout, cursor, renderer, clipboard, shell, viewport,
         )
     }
 }
 
-pub trait Layout<'a, Message, Theme, Renderer> {
+pub trait Layout<'a, Message, Theme, Renderer, State> {
     fn layout(
         &self,
+        state: &mut State,
         inner: &Element<'a, Message, Theme, Renderer>,
         tree: &mut iced::advanced::widget::Tree,
         renderer: &Renderer,
@@ -128,12 +145,13 @@ pub trait Layout<'a, Message, Theme, Renderer> {
     ) -> layout::Node;
 }
 
-impl<'a, Message, Theme, Renderer> Layout<'a, Message, Theme, Renderer> for ()
+impl<'a, Message, Theme, Renderer, State> Layout<'a, Message, Theme, Renderer, State> for ()
 where
     Renderer: advanced::Renderer + 'a,
 {
     fn layout(
         &self,
+        _state: &mut State,
         inner: &Element<'a, Message, Theme, Renderer>,
         tree: &mut iced::advanced::widget::Tree,
         renderer: &Renderer,
@@ -143,9 +161,10 @@ where
     }
 }
 
-impl<'a, T, Message, Theme, Renderer> Layout<'a, Message, Theme, Renderer> for T
+impl<'a, T, Message, Theme, Renderer, State> Layout<'a, Message, Theme, Renderer, State> for T
 where
     T: Fn(
+            &mut State,
             &Element<'a, Message, Theme, Renderer>,
             &mut iced::advanced::widget::Tree,
             &Renderer,
@@ -155,21 +174,23 @@ where
 {
     fn layout(
         &self,
+        state: &mut State,
         inner: &Element<'a, Message, Theme, Renderer>,
         tree: &mut iced::advanced::widget::Tree,
         renderer: &Renderer,
         limits: &iced::advanced::layout::Limits,
     ) -> layout::Node {
-        self(inner, tree, renderer, limits)
+        self(state, inner, tree, renderer, limits)
     }
 }
 
-impl<'a, Message, Theme, Renderer, OnEvent, Layout> Widget<Message, Theme, Renderer>
-    for Wrap<'a, Message, Theme, Renderer, OnEvent, Layout>
+impl<'a, Message, Theme, Renderer, OnEvent, Layout, State> Widget<Message, Theme, Renderer>
+    for Wrap<'a, Message, Theme, Renderer, OnEvent, Layout, State>
 where
     Renderer: advanced::Renderer,
-    OnEvent: self::OnEvent<'a, Message, Theme, Renderer>,
-    Layout: self::Layout<'a, Message, Theme, Renderer>,
+    OnEvent: self::OnEvent<'a, Message, Theme, Renderer, State>,
+    Layout: self::Layout<'a, Message, Theme, Renderer, State>,
+    State: Default + 'static,
 {
     fn size(&self) -> iced::Size<iced::Length> {
         self.inner.as_widget().size()
@@ -181,7 +202,13 @@ where
         renderer: &Renderer,
         limits: &iced::advanced::layout::Limits,
     ) -> iced::advanced::layout::Node {
-        self.layout.layout(&self.inner, tree, renderer, limits)
+        self.layout.layout(
+            tree.state.downcast_mut(),
+            &self.inner,
+            &mut tree.children[0],
+            renderer,
+            limits,
+        )
     }
 
     fn draw(
@@ -194,9 +221,15 @@ where
         cursor: iced::advanced::mouse::Cursor,
         viewport: &iced::Rectangle,
     ) {
-        self.inner
-            .as_widget()
-            .draw(tree, renderer, theme, style, layout, cursor, viewport)
+        self.inner.as_widget().draw(
+            &tree.children[0],
+            renderer,
+            theme,
+            style,
+            layout,
+            cursor,
+            viewport,
+        )
     }
 
     fn size_hint(&self) -> iced::Size<iced::Length> {
@@ -204,19 +237,20 @@ where
     }
 
     fn tag(&self) -> advanced::widget::tree::Tag {
-        self.inner.as_widget().tag()
+        struct Marker<State>(State);
+        advanced::widget::tree::Tag::of::<Marker<State>>()
     }
 
     fn state(&self) -> advanced::widget::tree::State {
-        self.inner.as_widget().state()
+        advanced::widget::tree::State::new(State::default())
     }
 
     fn children(&self) -> Vec<advanced::widget::Tree> {
-        self.inner.as_widget().children()
+        vec![advanced::widget::Tree::new(&self.inner)]
     }
 
     fn diff(&self, tree: &mut advanced::widget::Tree) {
-        self.inner.as_widget().diff(tree)
+        tree.diff_children(slice::from_ref(&self.inner));
     }
 
     fn operate(
@@ -228,12 +262,12 @@ where
     ) {
         self.inner
             .as_widget()
-            .operate(state, layout, renderer, operation)
+            .operate(&mut state.children[0], layout, renderer, operation)
     }
 
     fn on_event(
         &mut self,
-        state: &mut advanced::widget::Tree,
+        tree: &mut advanced::widget::Tree,
         event: iced::Event,
         layout: advanced::Layout<'_>,
         cursor: advanced::mouse::Cursor,
@@ -243,8 +277,9 @@ where
         viewport: &iced::Rectangle,
     ) -> advanced::graphics::core::event::Status {
         self.on_event.on_event(
+            tree.state.downcast_mut(),
             &mut self.inner,
-            state,
+            &mut tree.children[0],
             event,
             layout,
             cursor,
@@ -263,9 +298,13 @@ where
         viewport: &iced::Rectangle,
         renderer: &Renderer,
     ) -> advanced::mouse::Interaction {
-        self.inner
-            .as_widget()
-            .mouse_interaction(state, layout, cursor, viewport, renderer)
+        self.inner.as_widget().mouse_interaction(
+            &state.children[0],
+            layout,
+            cursor,
+            viewport,
+            renderer,
+        )
     }
 
     fn overlay<'b>(
@@ -277,21 +316,22 @@ where
     ) -> Option<advanced::overlay::Element<'b, Message, Theme, Renderer>> {
         self.inner
             .as_widget_mut()
-            .overlay(state, layout, renderer, translation)
+            .overlay(&mut state.children[0], layout, renderer, translation)
     }
 }
 
-impl<'a, Message, Theme, Renderer, OnEvent, Layout>
-    From<Wrap<'a, Message, Theme, Renderer, OnEvent, Layout>>
+impl<'a, Message, Theme, Renderer, OnEvent, Layout, State>
+    From<Wrap<'a, Message, Theme, Renderer, OnEvent, Layout, State>>
     for Element<'a, Message, Theme, Renderer>
 where
     Message: 'a,
     Theme: 'a,
     Renderer: advanced::Renderer + 'a,
-    OnEvent: self::OnEvent<'a, Message, Theme, Renderer> + 'a,
-    Layout: self::Layout<'a, Message, Theme, Renderer> + 'a,
+    OnEvent: self::OnEvent<'a, Message, Theme, Renderer, State> + 'a,
+    Layout: self::Layout<'a, Message, Theme, Renderer, State> + 'a,
+    State: Default + 'static,
 {
-    fn from(wrap: Wrap<'a, Message, Theme, Renderer, OnEvent, Layout>) -> Self {
+    fn from(wrap: Wrap<'a, Message, Theme, Renderer, OnEvent, Layout, State>) -> Self {
         Element::new(wrap)
     }
 }
