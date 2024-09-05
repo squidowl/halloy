f6133e3b3268977b65ebfa7c717ecd0f47cdd199
Andrew Baldwin <andrew@boldfriend.org>
2024-09-05 15:36:15 -0700

    Add `*` as an allowed first character of a nickname to support ZNC nicknames.

--- data/src/user.rs
+++ data/src/user.rs
@@ -65,9 +65,10 @@ impl<'a> TryFrom<&'a str> for User {
             return Err("nickname can't be empty");
         }
 
-        let Some(index) = value.find(|c: char| c.is_alphabetic() || "[\\]^_`{|}".find(c).is_some())
+        let Some(index) =
+            value.find(|c: char| c.is_alphabetic() || "[\\]^_`{|}*".find(c).is_some())
         else {
-            return Err("nickname must start with alphabetic or [ \\ ] ^ _ ` { | }");
+            return Err("nickname must start with alphabetic or [ \\ ] ^ _ ` { | } *");
         };
 
         let (access_levels, rest) = (&value[..index], &value[index..]);
