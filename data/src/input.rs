5069479787ea4cbbf5345af3c053de64e1d66b24
Andrew Baldwin <andrew@boldfriend.org>
2024-09-16 13:58:42 -0700

    Support for [message-ids](https://ircv3.net/specs/extensions/message-ids).

--- data/src/input.rs
+++ data/src/input.rs
@@ -96,6 +96,7 @@ impl Input {
                         direction: message::Direction::Sent,
                         target,
                         content: message::parse_fragments(text.clone(), channel_users),
+                        id: None,
                     })
                     .collect(),
             ),
@@ -105,6 +106,7 @@ impl Input {
                 direction: message::Direction::Sent,
                 target: to_target(&target, message::Source::Action)?,
                 content: message::action_text(user.nickname(), Some(&action)),
+                id: None,
             }]),
             _ => None,
         }
