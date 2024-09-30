cd98e456c01e32a98a393df3a9b3faf73348d43b
Casper Rogild Storm <casper+github@rogildstorm.com>
2024-09-30 23:03:04 +0200

    Updated icon

--- src/icon.rs
+++ src/icon.rs
@@ -72,6 +72,10 @@ pub fn popout<'a>() -> Text<'a> {
     to_text('\u{E80E}')
 }
 
+pub fn logs<'a>() -> Text<'a> {
+    to_text('\u{E810}')
+}
+
 fn to_text<'a>(unicode: char) -> Text<'a> {
     text(unicode.to_string())
         .line_height(LineHeight::Relative(1.0))
