2555fca964e3d269a9b20a0a430942ba72eaca86
Cory Forsstrom <cforsstrom18@gmail.com>
2025-02-12 13:56:33 -0800

    update Subscription::run_with

--- src/stream.rs
+++ src/stream.rs
@@ -1,7 +1,33 @@
+use std::hash::Hash;
+
 pub use data::stream::{self, *};
 use data::{config, server};
+use futures::Stream;
 use iced::Subscription;
 
 pub fn run(entry: server::Entry, proxy: Option<config::Proxy>) -> Subscription<stream::Update> {
-    Subscription::run_with(entry.server.clone(), stream::run(entry, proxy))
+    struct State {
+        entry: server::Entry,
+        proxy: Option<config::Proxy>,
+    }
+
+    impl State {
+        fn run(&self) -> impl Stream<Item = stream::Update> {
+            stream::run(self.entry.clone(), self.proxy.clone())
+        }
+    }
+
+    impl PartialEq for State {
+        fn eq(&self, other: &Self) -> bool {
+            self.entry.server.eq(&other.entry.server)
+        }
+    }
+
+    impl Hash for State {
+        fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+            self.entry.server.hash(state);
+        }
+    }
+
+    Subscription::run_with(State { entry, proxy }, State::run)
 }
