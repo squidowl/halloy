96db17165ae4777a2c43581817d4db3565bb79d7
Casper Rogild Storm <casper+github@rogildstorm.com>
2025-03-02 20:04:52 +0100

    persist buffer settings

--- data/src/dashboard.rs
+++ data/src/dashboard.rs
@@ -1,16 +1,32 @@
+use std::collections::HashMap;
 use std::io;
 use std::path::PathBuf;
 
 use serde::{Deserialize, Serialize};
 
 use crate::pane::Pane;
-use crate::{compression, environment};
+use crate::{buffer, compression, environment};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Dashboard {
     pub pane: Pane,
     #[serde(default)]
     pub popout_panes: Vec<Pane>,
+    #[serde(default)]
+    pub buffer_settings: BufferSettings,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct BufferSettings(HashMap<String, buffer::Settings>);
+
+impl BufferSettings {
+    pub fn get(&self, buffer: &buffer::Buffer) -> Option<&buffer::Settings> {
+        self.0.get(&buffer.key())
+    }
+
+    pub fn insert(&mut self, buffer: buffer::Buffer, settings: &buffer::Settings) {
+        self.0.insert(buffer.key(), settings.clone());
+    }
 }
 
 #[derive(Debug, Clone, Copy, Default, Deserialize, Serialize)]
