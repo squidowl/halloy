71df5d7410c317512bae08716101a82601d03074
Casper Rogild Storm <casper+github@rogildstorm.com>
2025-03-08 13:35:44 +0100

    Ability to quit Halloy with keybinds and command

--- src/screen/dashboard.rs
+++ src/screen/dashboard.rs
@@ -783,6 +783,9 @@ impl Dashboard {
                                     (window::toggle_fullscreen(), None)
                                 }
                             },
+                            command_bar::Command::Application(application) => match application {
+                                command_bar::Application::Quit => (iced::exit(), None),
+                            },
                         };
 
                         return (
@@ -970,6 +973,7 @@ impl Dashboard {
                         );
                     }
                     ToggleFullscreen => return (window::toggle_fullscreen(), None),
+                    QuitApplication => return (iced::exit(), None),
                 }
             }
             Message::FileTransfer(update) => {
