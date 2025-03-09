31fb1fd9030b59f7fb4c2a366c63248529d6e011
Casper Rogild Storm <casper+github@rogildstorm.com>
2025-03-09 23:27:13 +0100

    Update crates.

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     #[cfg(windows)]
     {
-        embed_resource::compile("assets/windows/halloy.rc", embed_resource::NONE);
+        let _ = embed_resource::compile("assets/windows/halloy.rc", embed_resource::NONE);
         windows_exe_info::versioninfo::link_cargo_env();
     }
 }
