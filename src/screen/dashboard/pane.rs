63ea06a22004216dd5336603c3352749be6c4c4b
Casper Rogild Storm <casper+github@rogildstorm.com>
2025-05-27 22:49:31 +0200

    show mode in panel

--- src/screen/dashboard/pane.rs
+++ src/screen/dashboard/pane.rs
@@ -60,12 +60,18 @@ impl Pane {
             Buffer::Empty => String::new(),
             Buffer::Channel(state) => {
                 let channel = state.target.as_str();
+
                 let server = &state.server;
                 let users = clients
                     .get_channel_users(&state.server, &state.target)
                     .len();
 
-                format!("{channel} @ {server} - {users} users")
+                let mode = clients
+                    .get_channel_mode(&state.server, &state.target)
+                    .map(|mode| format!(" ({mode})"))
+                    .unwrap_or_default();
+
+                format!("{channel}{mode} @ {server} - {users} users")
             }
             Buffer::Server(state) => state.server.to_string(),
             Buffer::Query(state) => {
