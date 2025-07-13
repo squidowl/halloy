60a1a157d04189e8231fb78b915f9cb4fd94eca2
James Liu <james@standof.fish>
2025-07-13 01:12:06 -0400

    Ensures filters correctly update open buffers on reload
    Under the hood
    ---
    
    History filters:
    - Moved core logic of building filters into `history/filters.rs`
    - Removed plain regex usage
    - Simplified `FilterTarget` to use `Source::User` only
    
    History manager:
    - Status of cached channels now tracked to prevent excessive rebuilding
    - Blocked message cache is now correctly generated if no cache is found
    during add_message
    
    Dashboard:
    - Correctly rebuilds cache by getting the channel info from opened panes
    
    Client:
    - Added `data::client::Event::AddedIsupportParam` to allow casemapping
    to be updated when server has information
    - Adds event handling to main.rs

--- data/src/history/filter.rs
+++ data/src/history/filter.rs
@@ -1,9 +1,9 @@
-use regex::Regex;
+use fancy_regex::Regex;
 use thiserror::Error;
 
 use crate::{
-    Message, Server, User, isupport,
-    message::{Content, Source},
+    Config, Message, Server, User, isupport,
+    message::Source,
     target::{Channel, Query},
     user::TryFromUserError,
 };
@@ -14,7 +14,6 @@ use super::Kind;
 pub struct Filter {
     pub target: FilterTarget,
     pub class: FilterClass,
-    // pub content: Option,
 }
 
 #[derive(Debug, Clone)]
@@ -26,20 +25,14 @@ pub enum FilterClass {
 
 #[derive(Debug, Clone)]
 pub enum FilterTarget {
-    User((Source, Source)),
-    // Regex(Regex),
+    User(Source),
     Any,
 }
 
 impl FilterTarget {
     pub fn user_try_from_str(nick: &str) -> Result<Self, FilterError> {
         User::try_from(nick)
-            .map(|u| {
-                Self::User((
-                    Source::User(u.clone()),
-                    Source::Action(Some(u.clone())),
-                ))
-            })
+            .map(|u| Self::User(Source::User(u.clone())))
             .map_err(FilterError::TryFromUserError)
     }
 }
@@ -48,22 +41,39 @@ impl FilterTarget {
 pub enum FilterError {
     #[error("Unable to generate filter from nickname {0:?}")]
     TryFromUserError(TryFromUserError),
-    // #[error("Unable to generate filter from regex \"{0}\", message: {1:?}")]
-    // RegexError(String, regex::Error),
 }
 
 impl Filter {
-    pub fn try_from_str_with_server(
+    pub fn list_from_config(config: &Config) -> Vec<Self> {
+        let mut new_filters = Vec::new();
+        config.servers.entries().for_each(|entry| {
+            let Some(filters) = &entry.config.filters else {
+                return;
+            };
+
+            for idx in 0..filters.ignore.len() {
+                if let Ok(filter) = Filter::try_from_str_with_server(
+                    &entry.server,
+                    &filters.ignore[idx],
+                ) {
+                    new_filters.push(filter);
+                }
+            }
+        });
+        new_filters
+    }
+
+    fn try_from_str_with_server(
         server: &Server,
         value: &str,
     ) -> Result<Self, FilterError> {
         let channel_test = Regex::new("^[#&][^\x07, ]{1,49}").expect("Failed to compile static regex, if you see this, something has gone wrong.");
-        let (class, target) = match channel_test.find(value) {
+        let (class, target) = match channel_test.find(value).expect("Failed to run static regex, if you see this, something has gone wrong.") {
             Some(channel_match) => {
                 let offset = channel_match.end();
                 let channel = Channel::from_str(
                     channel_match.as_str(),
-                    isupport::CaseMap::default(),
+                    isupport::CaseMap::default()
                 );
 
                 let target = match value.get(offset..) {
@@ -87,23 +97,14 @@ impl Filter {
     /// [`Message`]:crate::Message
     pub fn match_message(&self, message: &Message) -> bool {
         match &self.target {
-            FilterTarget::User((
-                Source::User(user),
-                Source::Action(action),
-            )) => match &message.target.source() {
-                Source::Action(msg_action) => msg_action == action,
-                Source::User(msg_user) => msg_user == user,
-                _ => match &message.content {
-                    Content::Fragments(fragments) => {
-                        fragments.iter().any(|frag| match frag {
-                            crate::message::Fragment::User(u, _) => u == user,
-                            _ => false,
-                        })
+            FilterTarget::User(Source::User(user)) => {
+                match &message.target.source() {
+                    Source::Action(Some(msg_user)) | Source::User(msg_user) => {
+                        msg_user == user
                     }
                     _ => false,
-                },
-            },
-            // FilterTarget::Regex(re) => re.is_match(&message.text()),
+                }
+            }
             FilterTarget::Any => true,
             _ => false,
         }
@@ -117,11 +118,12 @@ impl Filter {
     /// [`Query`]:crate::Query
     pub fn match_query(&self, query: &Query) -> bool {
         match &self.target {
-            FilterTarget::User((Source::User(user), ..)) => match &self.class {
+            FilterTarget::User(Source::User(user)) => match &self.class {
                 FilterClass::Channel((_, _)) => false,
                 _ => user.as_str() == query.as_str(),
             },
-            _ => false,
+            FilterTarget::User(_) => false,
+            FilterTarget::Any => false,
         }
     }
 
@@ -129,7 +131,7 @@ impl Filter {
         match &self.class {
             FilterClass::Server(target_server) => match kind {
                 Kind::Channel(server, _ch) => target_server == server,
-                // history::Kind::Server narrows to server messages only
+                // history::Kind::Server narrows to server messages only,
                 Kind::Server(server) => target_server == server,
                 Kind::Highlights => true,
                 _ => false,
@@ -143,7 +145,34 @@ impl Filter {
                 _ => false,
             },
             FilterClass::Any => true,
-            // _ => false,
+        }
+    }
+
+    pub fn match_server(&self, server: &Server) -> bool {
+        match &self.class {
+            FilterClass::Server(target_server)
+            | FilterClass::Channel((target_server, ..)) => {
+                target_server == server
+            }
+            FilterClass::Any => true,
+        }
+    }
+
+    fn sync_casemapping(
+        &mut self,
+        target_server: &Server,
+        casemapping: isupport::CaseMap,
+    ) {
+        let FilterClass::Channel((server, channel)) = &self.class else {
+            return;
+        };
+
+        if target_server == server {
+            let updated_channel =
+                Channel::from_str(channel.as_str(), casemapping);
+
+            self.class =
+                FilterClass::Channel((server.clone(), updated_channel));
         }
     }
 
@@ -174,6 +203,21 @@ impl<'f> FilterChain<'f> {
             .iter()
             .filter(|f| f.match_kind(kind))
             .any(|f| f.match_message(message))
+    }
+
+    pub fn sync_channels(
+        filters: &'f mut [Filter],
+        server: &Server,
+        casemapping: isupport::CaseMap,
+    ) {
+        log::debug!("updating casemap for {server:?}");
+        filters
+            .iter_mut()
+            .filter(|filter| filter.match_server(server))
+            .for_each(|filter| {
+                log::debug!("{filter:?}");
+                filter.sync_casemapping(server, casemapping);
+            });
     }
 
     pub fn filter_message(&self, message: &Message) -> bool {
