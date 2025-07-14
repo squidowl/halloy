1c11d488e14cec78c5e6090cb7b65342cf4f657f
Casper Rogild Storm <casper+github@rogildstorm.com>
2025-07-14 23:04:34 +0200

    Add configurable scrollbar sizing

--- data/src/config/pane.rs
+++ data/src/config/pane.rs
@@ -5,6 +5,9 @@ pub struct Pane {
     /// Default axis used when splitting a pane.
     #[serde(default)]
     pub split_axis: SplitAxis,
+    /// Scrollbar configuration
+    #[serde(default)]
+    pub scrollbar: Scrollbar,
 }
 
 #[derive(Debug, Copy, Clone, Deserialize, Default)]
@@ -13,4 +16,22 @@ pub enum SplitAxis {
     #[default]
     Horizontal,
     Vertical,
+}
+
+#[derive(Debug, Copy, Clone, Deserialize, Default)]
+pub struct Scrollbar {
+    /// Width of the scrollbar.
+    #[serde(default = "default_scrollbar_width")]
+    pub width: u32,
+    /// Width of the scrollbar scroller.
+    #[serde(default = "default_scrollbar_scroller_width")]
+    pub scroller_width: u32,
+}
+
+fn default_scrollbar_width() -> u32 {
+    5
+}
+
+fn default_scrollbar_scroller_width() -> u32 {
+    5
 }
