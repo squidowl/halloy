f0fb66f5aea6ab491afb8e8bc3279af15a5ff5bb
Andrew Baldwin <andrew@boldfriend.org>
2025-08-24 14:17:34 -0700

    Add `"restore_on_launch"` pane setting.

--- data/src/config/pane.rs
+++ data/src/config/pane.rs
@@ -2,12 +2,23 @@ use serde::Deserialize;
 
 use crate::config::Scrollbar;
 
-#[derive(Debug, Clone, Deserialize, Default)]
+#[derive(Debug, Clone, Deserialize)]
 #[serde(default)]
 pub struct Pane {
     /// Default axis used when splitting a pane.
     pub split_axis: SplitAxis,
     pub scrollbar: Scrollbar,
+    pub restore_on_launch: bool,
+}
+
+impl Default for Pane {
+    fn default() -> Self {
+        Self {
+            split_axis: SplitAxis::default(),
+            scrollbar: Scrollbar::default(),
+            restore_on_launch: true,
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, Deserialize, Default)]
