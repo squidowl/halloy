5b1e6d6b2eab275ca2922b866ccff65d7212437f
Andrew Baldwin <andrew@boldfriend.org>
2025-09-01 02:29:46 -0700

    Revert "Add (de)serialization tests for `Message`." (commit 42aa888e117d87519501569d29101d85c56ac4b1). Commit was accidentally brought along with rebase of PR #1106.

--- data/scripts/generate-message-tests-json.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env -S bash -e
-
-  script_dir=$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")
-
-  cd "$script_dir"/..
-
-  commit_hash=$(git rev-parse HEAD)
-
-  cargo run --features="message_tests" > tests/message/$commit_hash.json
