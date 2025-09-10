64e2a74e01979d2703fefe42f66ba0b7aff65019
Casper Rogild Storm <casper+github@rogildstorm.com>
2025-09-10 09:36:36 +0200

    Revert "Removed mundy in favor of iced built in."
    This reverts commit 3204640a900e2246292574000c0fca726f53ed1a.

--- src/appearance.rs
+++ src/appearance.rs
@@ -1,14 +1,28 @@
 use data::appearance;
+use futures::stream::BoxStream;
+use futures::StreamExt;
+use iced::advanced::graphics::futures::subscription;
+use iced::advanced::subscription::Hasher;
+use iced::{Subscription, futures};
+use mundy::{ColorScheme, Interest, Preferences};
 pub use theme::Theme;
 
 pub mod theme;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
-pub struct Mode(iced::theme::Mode);
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub enum Mode {
+    Dark,
+    Light,
+    Unspecified,
+}
 
-impl From<iced::theme::Mode> for Mode {
-    fn from(mode: iced::theme::Mode) -> Self {
-        Self(mode)
+impl From<ColorScheme> for Mode {
+    fn from(mode: ColorScheme) -> Self {
+        match mode {
+            ColorScheme::Dark => Mode::Dark,
+            ColorScheme::Light => Mode::Light,
+            ColorScheme::NoPreference => Mode::Unspecified,
+        }
     }
 }
 
@@ -16,12 +30,39 @@ impl Mode {
     pub fn theme(&self, selected: &data::appearance::Selected) -> data::appearance::Theme {
         match &selected {
             appearance::Selected::Static(theme) => theme.clone(),
-            appearance::Selected::Dynamic { light, dark } => match self.0 {
-                iced::theme::Mode::Dark => dark.clone(),
-                iced::theme::Mode::Light => light.clone(),
-                // We map `None` to `Light`.
-                iced::theme::Mode::None => light.clone()
+            appearance::Selected::Dynamic { light, dark } => match self {
+                Self::Dark => dark.clone(),
+                Self::Light => light.clone(),
+                // We map `Unspecified` to `Light`.
+                // This is because Gnome never specifies `Light` and only sends `Unspecified`.
+                Self::Unspecified => light.clone()
             },
         }
     }
+}
+
+struct Appearance;
+
+impl subscription::Recipe for Appearance {
+    type Output = Mode;
+
+    fn hash(&self, state: &mut Hasher) {
+        use std::hash::Hash;
+        struct Marker;
+        std::any::TypeId::of::<Marker>().hash(state);
+    }
+
+    fn stream(
+        self: Box<Self>,
+        _input: subscription::EventStream,
+    ) -> BoxStream<'static, Mode> {
+
+        Preferences::stream(Interest::ColorScheme)
+            .map(|preference| Mode::from(preference.color_scheme))
+            .boxed()
+    }
+}
+
+pub fn subscription() -> Subscription<Mode> {
+    subscription::from_recipe(Appearance)
 }
