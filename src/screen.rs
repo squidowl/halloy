a71c320dcf94b27f8c626b7a1bc29ebc1d9077ea
Casper Rogild Storm <casper+github@rogildstorm.com>
2025-10-28 22:55:48 +0100

    remove migration screen

--- src/screen.rs
+++ src/screen.rs
@@ -1,9 +1,7 @@
 pub mod dashboard;
 pub mod help;
-pub mod migration;
 pub mod welcome;
 
 pub use dashboard::Dashboard;
 pub use help::Help;
-pub use migration::Migration;
 pub use welcome::Welcome;
