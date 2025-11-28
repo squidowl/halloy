4a2e43926a55f67dfab051fba228ea47d4a0b841
Kai <kai@korla.cloud>
2025-11-28 17:12:10 +0100

    Upgrade WIX toolset

--- scripts/build-windows-installer.sh
+++ scripts/build-windows-installer.sh
@@ -6,10 +6,10 @@ HALLOY_VERSION=$(cat VERSION).0
 scripts/build-windows.sh
 
 # install latest wix
-dotnet tool install --global wix --version 5.0.2
+dotnet tool install --global wix --version 6.0.2
 
 # add required wix extension
-wix extension add WixToolset.UI.wixext/5.0.2
+wix extension add WixToolset.UI.wixext/6.0.2
 
 # build the installer
 wix build -pdbtype none -arch x64 -d PackageVersion=$HALLOY_VERSION $WXS_FILE -o target/release/halloy-installer.msi -ext WixToolset.UI.wixext
