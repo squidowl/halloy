329b2235751ea23cea892d705f604733d51b661e
andymandias <andymandias@users.noreply.github.com>
2025-12-09 16:33:35 +0000

    deploy: 91ece95c18310a28100f91c85531c7c6d52dcfaa

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0bb42b93.js',
+                path_to_root + 'searchindex-fd26ce67.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
