f37f796121ec6a0b546324adfc9255b1c818a785
Mykyta Holubakha <hilobakho@gmail.com>
2026-01-29 02:05:06 +0200

    icons: wire up font.line_height

--- src/widget.rs
+++ src/widget.rs
@@ -20,7 +20,7 @@ pub use self::selectable_text::selectable_text;
 pub use self::shortcut::shortcut;
 pub use self::tooltip::tooltip;
 use crate::appearance::theme::TEXT_SIZE;
-use crate::{Theme, font};
+use crate::{Theme, font, theme};
 
 pub mod anchored_overlay;
 pub mod color_picker;
@@ -78,7 +78,7 @@ where
         Marker::Dot => (
             "\u{E81A}",
             font_size,
-            LineHeight::default(),
+            theme::line_height(&config.font),
         ),
         Marker::Expand => {
             ("\u{E81B}", font_size * 0.75, LineHeight::Relative(1.75))
