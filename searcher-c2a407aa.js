fa865db80c737f0f566872fbfe03d3adf3ffbe99
casperstorm <casperstorm@users.noreply.github.com>
2026-02-14 11:04:30 +0000

    deploy: a6d9b7517f158a9801723ce9f2890666b2de76b5

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-703a2a57.js',
+                path_to_root + 'searchindex-fe0c47a3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
