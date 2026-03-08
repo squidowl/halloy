6ab6d696ab979dd1838bcc1b2675fdcff754692e
quaff <me@quaff.ca>
2026-03-08 13:59:02 -0700

    Scroll to top when clicking search toggle

--- book/scripts/move-searchbar.js
+++ book/scripts/move-searchbar.js
@@ -4,16 +4,30 @@ window.addEventListener("load", () => {
   const searchNode = document.querySelector("#mdbook-search-wrapper");
   const searchToggleNode = document.querySelector("#mdbook-search-toggle");
 
+  // initalize search with simulated clicks
+  if (searchToggleNode) {
+    // first click simulates initializing search
+    searchToggleNode.click();
+    // second simulated click enables user's initial click of search toggle
+    searchToggleNode.click();
+  }
+
+  // remove focus from the search input
+  if (searchNode) {
+    setTimeout(() => searchNode.querySelector("input").blur(), 50);
+  }
+
   // move search bar to the sidebar
   if (sidebarNode && searchNode) {
     sidebarNode.prepend(searchNode);
   }
 
-  // first click simulates initializing search
-  searchToggleNode.click();
-  // second simulated click enables user's initial click of search toggle
-  searchToggleNode.click();
-
-  // remove focus from the search input
-  setTimeout(() => searchNode.querySelector("input").blur(), 50);
+  // scroll sidebar to top on search toggle
+  if (sidebarNode && searchToggleNode) {
+    setTimeout(() => {
+      searchToggleNode.addEventListener("click", () => {
+        sidebarNode.scrollTop = 0;
+      });
+    }, 100);
+  }
 });
