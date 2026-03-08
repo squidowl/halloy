27277adc5757621be958b5d9783dba05b67c9081
quaff <me@quaff.ca>
2026-03-08 14:07:39 -0700

    Search toggle behaviour on blur

--- book/scripts/move-searchbar.js
+++ book/scripts/move-searchbar.js
@@ -4,30 +4,33 @@ window.addEventListener("load", () => {
   const searchNode = document.querySelector("#mdbook-search-wrapper");
   const searchToggleNode = document.querySelector("#mdbook-search-toggle");
 
+  // move search bar to the sidebar
+  if (sidebarNode && searchNode) {
+    sidebarNode.prepend(searchNode);
+  }
+
   // initialize search with simulated clicks
   if (searchToggleNode) {
     // first click simulates initializing search
     searchToggleNode.click();
     // second simulated click enables user's initial click of search toggle
     searchToggleNode.click();
+
+    // mimic escape on blur
+    searchToggleNode.addEventListener("blur", () => {
+      searchToggleNode.click();
+    });
+
+    // scroll sidebar to top on search toggle
+    if (sidebarNode) {
+      searchToggleNode.addEventListener("click", () => {
+        sidebarNode.scrollTop = 0;
+      });
+    }
   }
 
   // remove focus from the search input
   if (searchNode) {
     setTimeout(() => searchNode.querySelector("input").blur(), 50);
-  }
-
-  // move search bar to the sidebar
-  if (sidebarNode && searchNode) {
-    sidebarNode.prepend(searchNode);
-  }
-
-  // scroll sidebar to top on search toggle
-  if (sidebarNode && searchToggleNode) {
-    setTimeout(() => {
-      searchToggleNode.addEventListener("click", () => {
-        sidebarNode.scrollTop = 0;
-      });
-    }, 100);
   }
 });
