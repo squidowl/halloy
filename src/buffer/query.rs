a2613dabc9f00652d3cdc73a1bfd216450b9b724
Casper Rogild Storm <casper+github@rogildstorm.com>
2026-04-02 08:47:06 +0200

    clean up

--- src/buffer/query.rs
+++ src/buffer/query.rs
@@ -124,9 +124,7 @@ pub fn view<'a>(
     let typing = typing::view(
         typing_text,
         state.typing_animation.as_ref(),
-        typing::typing_font_size(config),
-        config.buffer.line_spacing,
-        &config.buffer.typing.animation,
+        config,
         theme,
     );
 
