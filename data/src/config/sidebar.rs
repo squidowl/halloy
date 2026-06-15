e3cc8293cae8675e021387c067be4432dae30144
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-06-15 20:06:37 +0200

    Allow to show internal buffers only if unread

--- data/src/config/sidebar.rs
+++ data/src/config/sidebar.rs
@@ -41,8 +41,17 @@ pub struct Sidebar {
 #[derive(Debug, Clone, Deserialize, Default)]
 #[serde(default)]
 pub struct InternalBuffers {
+    pub show: InternalBuffersShowPolicy,
     pub position: InternalBufferPosition,
     pub buffers: Vec<InternalBuffer>,
+}
+
+#[derive(Debug, Copy, Clone, Deserialize, Default)]
+#[serde(rename_all = "kebab-case")]
+pub enum InternalBuffersShowPolicy {
+    #[default]
+    Always,
+    Unread,
 }
 
 impl InternalBuffers {
