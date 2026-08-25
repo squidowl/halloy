78eeb15d041db968ebfd3a1bf6dfb0cb2a205e58
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-08-25 16:48:39 +0200

    Stop looping the chennel logger if the other side dies
    It's not a possible source of deadlock since `std::thread` will
    terminate if we ever call `exit`, but \o/

--- src/logger.rs
+++ src/logger.rs
@@ -172,8 +172,16 @@ fn channel_logger() -> (Box<dyn Log>, ReceiverStream<Vec<Record>>) {
         let mut timeout = Instant::now();
 
         loop {
-            if let Ok(log) = log_receiver.recv_timeout(BATCH_TIMEOUT) {
-                batch.push(log);
+            match log_receiver.recv_timeout(BATCH_TIMEOUT) {
+                Ok(log) => batch.push(log),
+                Err(mpsc::RecvTimeoutError::Timeout) => {}
+                Err(mpsc::RecvTimeoutError::Disconnected) => {
+                    if !batch.is_empty() {
+                        let _ =
+                            async_sender.blocking_send(mem::take(&mut batch));
+                    }
+                    break;
+                }
             }
 
             if batch.len() >= BATCH_SIZE
@@ -181,10 +189,15 @@ fn channel_logger() -> (Box<dyn Log>, ReceiverStream<Vec<Record>>) {
             {
                 timeout = Instant::now();
 
-                let _ = async_sender.blocking_send(mem::replace(
-                    &mut batch,
-                    Vec::with_capacity(BATCH_SIZE),
-                ));
+                if async_sender
+                    .blocking_send(mem::replace(
+                        &mut batch,
+                        Vec::with_capacity(BATCH_SIZE),
+                    ))
+                    .is_err()
+                {
+                    break;
+                }
             }
         }
     });
