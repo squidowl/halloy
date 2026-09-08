00c27e7fe68762339655164215a0491b87179093
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-09-08 23:29:45 +0200

    Remove unneeded .line_height(...)

--- src/screen/dashboard/command_bar.rs
+++ src/screen/dashboard/command_bar.rs
@@ -88,7 +88,6 @@ impl CommandBar {
         // 1px larger than default
         let font_size =
             config.font.size.map_or(theme::TEXT_SIZE, f32::from) + 1.0;
-        let line_height = theme::line_height(&config.font);
 
         let combo_box =
             combo_box(&self.state, "Type a command...", None, Message::Command)
@@ -96,7 +95,6 @@ impl CommandBar {
                 .on_close(Message::Unfocused)
                 .on_option_hovered(Message::Hovered)
                 .size(font_size)
-                .line_height(line_height)
                 .padding([8, 8]);
 
         // Capture ESC so we can close the combobox manually from application
@@ -111,31 +109,25 @@ impl CommandBar {
         double_pass(
             // Layout should be based on the Shrink text size width of largest option
             column(
-                std::iter::once(
-                    text("Type a command...")
-                        .size(font_size)
-                        .line_height(line_height),
-                )
-                .chain(
-                    Command::list(
-                        servers,
-                        clients,
-                        buffers,
-                        config,
-                        focus,
-                        resize_buffer,
-                        version,
-                        main_window,
-                        show_muted_buffers,
+                std::iter::once(text("Type a command...").size(font_size))
+                    .chain(
+                        Command::list(
+                            servers,
+                            clients,
+                            buffers,
+                            config,
+                            focus,
+                            resize_buffer,
+                            version,
+                            main_window,
+                            show_muted_buffers,
+                        )
+                        .iter()
+                        .map(|command| {
+                            text(command.to_string()).size(font_size)
+                        }),
                     )
-                    .iter()
-                    .map(|command| {
-                        text(command.to_string())
-                            .size(font_size)
-                            .line_height(line_height)
-                    }),
-                )
-                .map(Element::from),
+                    .map(Element::from),
             )
             // Give it some extra width
             .padding([0, 20]),
